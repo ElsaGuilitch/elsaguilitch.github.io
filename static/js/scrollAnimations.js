9aa471503dca5f478d8a0c3f38fe018f23b77767
Elsa Guilitch <elsa.guilitch@gmail.com>
2020-03-31 12:38:35 +0200

    🎨 Reformat the whole project using prettier

--- static/js/scrollAnimations.js
+++ static/js/scrollAnimations.js
@@ -1,32 +1,34 @@
-function animateLaptop(node){
-  var watcher = scrollMonitor.create(node, -200);
-  var screen = $(node).find('.website');
+function animateLaptop(node) {
+  var watcher = scrollMonitor.create(node, -200)
+  var screen = $(node).find(".website")
 
-  watcher.enterViewport(function(){
-    node.addClass('show');
-  });
+  watcher.enterViewport(function() {
+    node.addClass("show")
+  })
 }
 
 // -----------------------------------------------------------------------------
 // Images
 
-function loadImages(nodes){
-  nodes.each(function(i, node){
-    var watcher = scrollMonitor.create(node, {top: -100});
+function loadImages(nodes) {
+  nodes.each(function(i, node) {
+    var watcher = scrollMonitor.create(node, {
+      top: -100
+    })
 
-    watcher.enterViewport(function(){
-      nodes.queue(function(){
-        $(node).addClass('show');
-        $(this).dequeue();
-      }).delay(150);
-    });
-  });
+    watcher.enterViewport(function() {
+      nodes
+        .queue(function() {
+          $(node).addClass("show")
+          $(this).dequeue()
+        })
+        .delay(150)
+    })
+  })
 }
 
-function loadImage(node){
-  var watcher = scrollMonitor.create(node);
+function loadImage(node) {
+  var watcher = scrollMonitor.create(node)
 
-  watcher.enterViewport(function(){
-
-  });
+  watcher.enterViewport(function() {})
 }
